750914d0f8c8d2557eed97c18deffdba3da46f1e
Lorenzo Lingardo <lorenzolingardo@Lorenzos-MacBook-Pro.local>
2021-11-10 15:21:48 -0700

    Add small fixes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import './App.css';
-import Header from "./Header"
-import Footer from "./Footer"
-import MainContent from "./MainContent"
-import DropDownMain from "./DropDownMain"
-import CFHistory from "./CFHistory"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import MainContent from "./components/MainContent"
+import DropDownMain from "./components/DropDownMain"
+import CFHistory from "./components/CFHistory"
 
 
 function App() {
   const cfFacts = CFHistory.map(content => <MainContent key = {content.id} fact={content.fact}/>)
-  var title = document.querySelector('title');
+  let title = document.querySelector('title');
   title.innerText = 'Clark Historic Farm';
   
   return (
