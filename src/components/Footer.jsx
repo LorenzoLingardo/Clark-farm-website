1a68ffc166c1680176325a3aa987d5137967d5fe
Lorenzo Lingardo <lorenzolingardo@Lorenzos-MacBook-Pro.local>
2021-11-16 14:29:00 -0700

    Change file types

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <h4 className="footerLeft">
+                Something
+            </h4>
+            <h5 className="footerRight">
+                Something smaller
+            </h5>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1 className="headerTitle">
+                Test title
+            </h1>
+            <h3 className="headerSubtitle">
+                Something
+            </h3>
+        </div>
+    )
+}
+
+export default Header;
