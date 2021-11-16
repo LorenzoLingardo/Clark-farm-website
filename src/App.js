ce6772b910777ec64e6beae638ac565918c6f93c
Lorenzo Lingardo <lorenzolingardo@Lorenzos-MacBook-Pro.local>
2021-11-16 12:23:29 -0700

    Saved over the wrong stuff but needed to reset so that works

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import React from 'react'
 import './App.css';
-import Header from "./Header"
-import Footer from "./Footer"
-import MainContent from "./MainContent"
-import DropDownMain from "./DropDownMain"
-import CFHistory from "./CFHistory"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Main from "./components/Main"
+
 
 
 function App() {
-  const cfFacts = CFHistory.map(content => <MainContent key = {content.id} fact={content.fact}/>)
   var title = document.querySelector('title');
   title.innerText = 'Clark Historic Farm';
   
   return (
     <div>
       <Header />
-      <DropDownMain/>
-      {cfFacts}
+      <Main />
       <Footer />
     </div>
   );
