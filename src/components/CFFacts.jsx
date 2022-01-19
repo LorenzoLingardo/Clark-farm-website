b05f330266b9c697c7131b554704711246855d4b
Lorenzo Lingardo <lorenzolingardo@Lorenzos-MacBook-Pro.local>
2022-01-19 14:00:54 -0700

    Fix Events and Tabs alignment

--- src/components/CFFacts.jsx
+++ src/components/CFFacts.jsx
@@ -1,13 +1,5 @@
 import React from "react"
 
-const CFFacts = props => {
-    return (
-        <div className="cfFacts">
-            <h1 className="factsContent fs-400 bg-white">
-                {props.fact}
-            </h1>
-        </div>
-    )
-}
+const CFFacts = props => <div> {props.fact} </div>
 
 export default CFFacts;
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -5,39 +5,20 @@ import Footer from "./Footer"
 import Main from "./Main"
 import CovidHeader from './COVIDHeader';
 import CFHistory from './CFHistory'
-import CFFacts from './CFFacts'
 import CFNews from './CFNews';
-import {Routes, Route} from "react-router-dom"
 import Survey from './Survey'
-import News from './News'
 
-function MainPage() {
+const MainPage = () => {
     var title = document.querySelector('title');
     title.innerText = 'Clark Historic Farm';
-    const cfFacts = CFHistory.map(content => {
-      return (
-        <CFFacts 
-          key={content.id} 
-          {...content} 
-        />)
-    })
-
-    const cfNews = CFNews.map(news => {
-      return (
-        <News 
-          key={news.id}
-          {...news} 
-        />
-      )
-    })
 
     return (
       <div className='parallax'>
         <CovidHeader />
         <Header/>
         <Main />
-        {/* {cfNews}  */}
-        {cfFacts}
+        {/* <CFNews/> */}
+        <CFHistory/>
         <Survey/>
         <Footer />
       </div>
