59158d136924ffbe832df5023b95df41f56dcd8b
Lorenzo Lingardo <lorenzolingardo@Lorenzos-MacBook-Pro.local>
2022-01-24 14:53:26 -0700

    Add routes to the header so navigation works

---
+++ src/components/educationpage/Education.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Footer from "../../Footer";
+import Header from "../../Header";
+
+const Education = () => {
+    return(
+        <div>
+            <Header/>
+            <h1>
+                Education
+            </h1>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Education; 
---
+++ src/components/volunteerpage/Volunteer.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import Footer from "../../Footer";
+import Header from "../../Header";
+
+const Volunteer = () => {
+    return(
+        <div>
+            <Header/>
+            <h1>
+                Volunteer
+            </h1>
+            <Footer/>
+        </div>
+    )
+}
+
+export default Volunteer; 
