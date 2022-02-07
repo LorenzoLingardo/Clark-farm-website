6ce11084d08535b8a6dc21b87200d9a22a324dad
Lorenzo Lingardo <lorenzolingardo@Lorenzos-MacBook-Pro.local>
2022-02-07 16:02:39 -0700

    Fix main page image and COVID banner

--- src/components/mainpage/COVIDBanner.jsx
+++ src/components/mainpage/COVIDBanner.jsx
@@ -11,7 +11,8 @@ const CovidBanner = () => {
         <div className="ff-sans-cond bg-accent fs-300 text-dark">
             <div>
                 <i class="fas fa-info-circle"></i>
-                The safety of our visitors and volunteers is important to us. For information about our COVID-19 policies, click<Button className="modalBtn" style={{"text-decoration": "none", "font-size": "var(--fs-300)"}} onClick={handleShow} variant="link">here</Button>
+                    {`The safety of our visitors and volunteers is important to us. For information about our COVID-19 policies, click `}
+                <Button style={{"text-decoration": "none", "font-size": "var(--fs-300)", "padding": "0px"}} onClick={handleShow} variant="link">here</Button>
             </div>
             <Modal show={show} onHide={handleClose} animation={false} id="covidModal">
                 <Modal.Header closeButton>
