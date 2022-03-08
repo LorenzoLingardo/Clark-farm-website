8f411349f22c1330f2d5502ac2ce6a3f9176e85d
Lorenzo Lingardo <lorenzolingardo@Lorenzos-MacBook-Pro.local>
2022-03-08 16:24:50 -0700

    Commit Firebase stuff

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvH9u2tHp6mTVuWOdri-fAlZjp79yk9Us",
+  authDomain: "clarkhistoricfarm-31b32.firebaseapp.com",
+  projectId: "clarkhistoricfarm-31b32",
+  storageBucket: "clarkhistoricfarm-31b32.appspot.com",
+  messagingSenderId: "862341147797",
+  appId: "1:862341147797:web:4aca4ee146a4cbd4c9bf9c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
