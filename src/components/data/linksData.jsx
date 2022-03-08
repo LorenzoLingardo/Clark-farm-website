4e5bbbe18074597271c03a0c1c1caba4ed06cd48
Lorenzo Lingardo <lorenzolingardo@Lorenzos-MacBook-Pro.local>
2022-03-08 12:37:02 -0700

    Change file formats

--- src/components/data/linksData.jsx
+++ src/components/data/linksData.jsx
@@ -1,34 +1,34 @@
 const linksData = [
-    {
-        id: "1",
-        title: "Event 1",
-        eventImg: "https://placeimg.com/640/480/nature",
-    },
-    {
-        id: "2",
-        title: "Event 2",
-        eventImg: "https://placeimg.com/640/480/nature",
-    },
-    {
-        id: "3",
-        title: "Event 3",
-        eventImg: "https://placeimg.com/640/480/nature",
-    },
-    {
-        id: "4",
-        title: "Event 4",
-        eventImg: "https://placeimg.com/640/480/nature",
-    },
-    {
-        id: "5",
-        title: "Event 5",
-        eventImg: "https://placeimg.com/640/480/nature",
-    },
-    {
-        id: "6",
-        title: "Event 6",
-        eventImg: "https://placeimg.com/640/480/nature",
-    }, 
-]
+  {
+    id: "1",
+    title: "Event 1",
+    eventImg: "https://placeimg.com/640/480/nature",
+  },
+  {
+    id: "2",
+    title: "Event 2",
+    eventImg: "https://placeimg.com/640/480/nature",
+  },
+  {
+    id: "3",
+    title: "Event 3",
+    eventImg: "https://placeimg.com/640/480/nature",
+  },
+  {
+    id: "4",
+    title: "Event 4",
+    eventImg: "https://placeimg.com/640/480/nature",
+  },
+  {
+    id: "5",
+    title: "Event 5",
+    eventImg: "https://placeimg.com/640/480/nature",
+  },
+  {
+    id: "6",
+    title: "Event 6",
+    eventImg: "https://placeimg.com/640/480/nature",
+  },
+];
 
 export default linksData;
--- src/components/mainpage/MainOptionsDesc.jsx
+++ src/components/mainpage/MainOptionsDesc.jsx
@@ -1,33 +1,13 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import MainOptions from "./MainOptions";
-
-const MainOptionsDescArray = [
-  {
-    id: 1,
-    title: "Upcoming Events",
-    img: "https://placeimg.com/640/480/nature",
-    link: "events",
-  },
-  {
-    id: 2,
-    title: "Volunteer",
-    img: "https://placeimg.com/640/480/animals",
-    link: "volunteer",
-  },
-  {
-    id: 3,
-    title: "About Us",
-    img: "https://placeimg.com/640/480/nature",
-    link: "aboutus",
-  },
-];
+import mainOptionsData from "../data/mainOptionsData";
 
 const MainOptionsDesc = () => {
   return (
     <Container className="ff-sans-cond my-4">
       <Row>
-        {MainOptionsDescArray.map((content) => {
+        {mainOptionsData.map((content) => {
           return (
             <Col sm>
               <div className="main-options-content bg-white mb-3 mx-3">
